e5af36aa490c74c445f3614dce320dd9ed2fef50
Carlos Aguilera <18aguilerac@gmail.com>
2022-07-15 16:37:58 -0700

    setting up route calls

--- controllers/api.js
+++ controllers/api.js
@@ -3,4 +3,20 @@ module.exports = class API {
   static async fetchAllPost(req, res) {
     res.send("HELLO FROM API");
   }
+  // fetch post by ID
+  static async fetchPostByID(req, res) {
+    res.send("Fetch Post By ID");
+  }
+  // Create a Post
+  static async createPost(req, res) {
+    res.send("Create Post");
+  }
+  // Update a Post
+  static async updatePost(req, res) {
+    res.send("Update Post");
+  }
+  // Delete a Post
+  static async deletePost(req, res) {
+    res.send("Delete Post");
+  }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const API = require("../controllers/api");
 
 router.get("/", API.fetchAllPost);
+router.get("/:id", API.fetchPostByID);
+router.post("/", API.createPost);
+router.patch("/:id", API.updatePost);
+router.delete("/:id", API.deletePost);
 
 module.exports = router;
