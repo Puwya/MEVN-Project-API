e5fcf364e1bae84855c8fcb833fc691a020739bf
Carlos Aguilera <18aguilerac@gmail.com>
2022-07-15 17:16:50 -0700

    fixed naming convention

--- controllers/api.js
+++ controllers/api.js
@@ -1,22 +1,29 @@
+const posting = require("../models/posting");
+
 module.exports = class API {
   // fetch all posts
-  static async fetchAllPost(req, res) {
-    res.send("HELLO FROM API");
+  static async fetchAllPostings(req, res) {
+    try {
+      const postings = await posting.find();
+      res.status(200).json(postings);
+    } catch (err) {
+      res.send(404).json({ message: err.message });
+    }
   }
   // fetch post by ID
-  static async fetchPostByID(req, res) {
+  static async fetchPostingByID(req, res) {
     res.send("Fetch Post By ID");
   }
   // Create a Post
-  static async createPost(req, res) {
+  static async createPosting(req, res) {
     res.send("Create Post");
   }
   // Update a Post
-  static async updatePost(req, res) {
+  static async updatePosting(req, res) {
     res.send("Update Post");
   }
   // Delete a Post
-  static async deletePost(req, res) {
+  static async deletePosting(req, res) {
     res.send("Delete Post");
   }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const API = require("../controllers/api");
 
-router.get("/", API.fetchAllPost);
-router.get("/:id", API.fetchPostByID);
-router.post("/", API.createPost);
-router.patch("/:id", API.updatePost);
-router.delete("/:id", API.deletePost);
+router.get("/", API.fetchAllPostings);
+router.get("/:id", API.fetchPostingByID);
+router.post("/", API.createPosting);
+router.patch("/:id", API.updatePosting);
+router.delete("/:id", API.deletePosting);
 
 module.exports = router;
